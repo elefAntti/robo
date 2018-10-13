8323692cc137f06dce879ee2598047989ee5a8d6
Unknown <antti.valli@gmail.com>
2018-10-13 14:02:43 +0300

    Fix receiving messages + listen on all interfaces

--- manual_drive.py
+++ manual_drive.py
@@ -5,7 +5,8 @@ from time import sleep
 import socket
 import sys
 
-ip = '169.254.130.192'
+#ip = '169.254.130.192'
+ip = ''
 forwardSpeed = 360
 turnBigSpeed = 180
 turnSmallSpeed = -180
@@ -17,34 +18,35 @@ server_address = (ip, 8000)
 
 sock.bind(server_address)
 
-#motorA = ev3.LargeMotor('outA')
-#motorB = ev3.LargeMotor('outB')
+motorA = ev3.LargeMotor('outA')
+motorB = ev3.LargeMotor('outB')
 button = ev3.Button()
 
 print("Connected")
 
 while not button.any():
     data, server = sock.recvfrom(64)
+    data = data.decode("utf-8")
     if data == 'forw':
         print('forw')
-        #motorA.run_forever(speed_sp = forwardSpeed)
-        #motorB.run_forever(speed_sp = forwardSpeed)
+        motorA.run_forever(speed_sp = forwardSpeed)
+        motorB.run_forever(speed_sp = forwardSpeed)
     elif data == 'back':
         print('back')
-        #motorA.run_forever(speed_sp = -forwardSpeed)
-        #motorB.run_forever(speed_sp = -forwardSpeed)
+        motorA.run_forever(speed_sp = -forwardSpeed)
+        motorB.run_forever(speed_sp = -forwardSpeed)
     elif data == 'left':
         print('left')
-        #motorA.run_forever(speed_sp = turnBigSpeed)
-        #motorB.run_forever(speed_sp = turnSmallSpeed)
+        motorA.run_forever(speed_sp = turnBigSpeed)
+        motorB.run_forever(speed_sp = turnSmallSpeed)
     elif data == 'right':
         print('right')
-        #motorA.run_forever(speed_sp = turnSmallSpeed)
-        #motorB.run_forever(speed_sp = turnBigSpeed)
+        motorA.run_forever(speed_sp = turnSmallSpeed)
+        motorB.run_forever(speed_sp = turnBigSpeed)
     else:
         print('halt')
-        #motorA.stop()
-        #motorB.stop()
+        motorA.stop()
+        motorB.stop()
 
-#motorA.stop()
-#motorB.stop()
+motorA.stop()
+motorB.stop()
