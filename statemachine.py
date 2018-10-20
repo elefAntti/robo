cfc283d74dec17698d91951a1eccbb245db4bf13
lauri.lammi <lauri.lammi@thermofisher.com>
2018-10-20 15:49:19 +0300

    Added missing King of the Hill state
    - Our numbering does not match the one used by the event
    - King of the Hill is Challenge 7
    - Added clarifying comments

--- statemachine.py
+++ statemachine.py
@@ -12,11 +12,13 @@ class Statemachine:
         self.stateChallenge4 = State(States.CHALLENGE4, environment)
         self.stateChallenge5 = State(States.CHALLENGE5, environment)
         self.stateChallenge6 = State(States.CHALLENGE6, environment)
+        self.stateChallenge7 = State(States.CHALLENGE7, environment)
         self.stateTransit12 = State(States.TRANSIT12, environment)
         self.stateTransit23 = State(States.TRANSIT23, environment)
         self.stateTransit34 = State(States.TRANSIT34, environment)
         self.stateTransit45 = State(States.TRANSIT45, environment)
         self.stateTransit56 = State(States.TRANSIT56, environment)
+        self.stateTransit67 = State(States.TRANSIT67, environment)
 
         self.states[States.MANUAL] = self.stateManual
         self.states[States.CHALLENGE1] = self.stateChallenge1
@@ -25,11 +27,13 @@ class Statemachine:
         self.states[States.CHALLENGE4] = self.stateChallenge4
         self.states[States.CHALLENGE5] = self.stateChallenge5
         self.states[States.CHALLENGE6] = self.stateChallenge6
+        self.states[States.CHALLENGE7] = self.stateChallenge7
         self.states[States.TRANSIT12] = self.stateTransit12
         self.states[States.TRANSIT23] = self.stateTransit23
         self.states[States.TRANSIT34] = self.stateTransit34
         self.states[States.TRANSIT45] = self.stateTransit45
         self.states[States.TRANSIT56] = self.stateTransit56
+        self.states[States.TRANSIT67] = self.stateTransit67
 
         self.stateChallenge1.NextState = States.TRANSIT12
         self.stateTransit12.NextState = States.CHALLENGE2
@@ -41,6 +45,8 @@ class Statemachine:
         self.stateTransit45.NextState = States.CHALLENGE5
         self.stateChallenge5.NextState = States.TRANSIT56
         self.stateTransit56.NextState = States.CHALLENGE6
+        self.stateChallenge6.NextState = States.TRANSIT67
+        self.stateTransit67.NextState = States.CHALLENGE7
 
         self.currentState = self.stateManual
         self.currentState.Enter()        
