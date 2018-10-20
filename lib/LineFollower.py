42696057e6f7d6eb1ac7319d7afcb8144a6c8e93
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-10-20 18:27:55 +0300

    code baseline with gyros

--- lib/LineFollower.py
+++ lib/LineFollower.py
@@ -1,5 +1,7 @@
 from lib.state import State, States
 from lib.enums import Colors
+from lib import RobotInterface
+import time
 
 class LineFollower(State):
 
@@ -11,23 +13,45 @@ class LineFollower(State):
         self._turn = 200
         self._bright = 15
         self._tspd = -120
+        self._gyroTreshold = 20
+        self._turnRight = True
+        self._lastGyro = 0
+        self._turnMode = False
 
     def Update(self):
-        print(self._colorSensor.value())        
-        rspd = 0
-        if self._colorSensor.value() > self._bright:
-            rspd = -self._turn
+        if self._turnMode:
+            operation = RobotInterface.GyroPivot(self._robot, 90 if self._turnRight else -90)
+            if operation.update():
+                self._turnMode = False
         else:
-            rspd = self._turn
+            print(self._colorSensor.value())        
+            rspd = 0
+            if self._colorSensor.value() > self._bright:
+                rspd = -self._turn
+                if not self._turnRight:
+                    self._turnRight = True
+                    self._lastGyro = self._gyro.value()
+            else:
+                rspd = self._turn
+                if self._turnRight:
+                    self._turnRight = False
+                    self._lastGyro = self._gyro.value()
 
-        right = (self._tspd + rspd)/2.0
-        left = self._tspd - right
-        self._robot.SimpleDrive(left, right)
+            if abs(self._lastGyro - self._gyro.value()) > self._gyroTreshold:
+                self._robot.driveForTime(500, 500, 0.25)
+                self._turnMode = True
+            right = (self._tspd + rspd)/2.0
+            left = self._tspd - right
+            self._robot.SimpleDrive(left, right)
 
         return self.Id
 
     def Enter(self):
         self._colorSensor.mode = 'COL-REFLECT'
+        self._gyro.mode = 'GYRO-ANG'
+        self._turnRight = True
+        self._lastGyro = self._gyro.value()
+        self._turnMode = False
 
     def Exit(self):
         self._robot.stop()
