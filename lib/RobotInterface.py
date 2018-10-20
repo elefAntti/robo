2499a561709b2d5c496bfa37f2e30715cd373497
Antti Valli <antti.valli@gmail.com>
2018-10-20 11:18:18 +0300

    Add gyro init and logging to robot Interface

--- lib/RobotInterface.py
+++ lib/RobotInterface.py
@@ -1,12 +1,29 @@
 from ev3dev import ev3
+import time
 
 class RobotInterface:
     
     def __init__(self, left_port, right_port, max_speed = 700, flip_dir=False):
         self.left_motor = ev3.LargeMotor(left_port)
         self.right_motor = ev3.LargeMotor(right_port)
+        self.sound = ev3.Sound()
+        try:
+            self.sound.beep()
+            time.sleep(2)
+            self.gyro = ev3.GyroSensor()
+            self.gyro.mode='GYRO-ANG'
+            time.sleep(2)
+            self.sound.beep()
+        except:
+            self.gyro = None
+            print("Gyro not found")
         self.max_speed = max_speed
         self.flip_dir = flip_dir
+        self.log = open("sensor.log", "w+")
+
+    def logStuff(self):
+        log.write("%f, %f, %f"% \
+            (self.gyro.value(), self.left_motor.position, self.right_motor.position))
 
     def simpleDrive(self, left_speed, right_speed):
         limiting_speed = max(abs(left_speed), abs(right_speed))
