b1f5f1e5e27de483b9a899de08df3f379a96c1a0
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-10-20 23:08:38 +0300

    pseudo code turned into actual code

--- lib/stateChallenge3.py
+++ lib/stateChallenge3.py
@@ -1,5 +1,6 @@
 from lib.state import State, States
 from lib.enums import Colors
+import RobotInterface
 
 class StateChallenge3(State):
 
@@ -18,23 +19,42 @@ class StateChallenge3(State):
 
         if self._rightPushSensor.is_pressed() and self._leftPushSensor.is_pressed():
             #aja taakse noin 5cm
+            command = RobotInterface.DriveForward(self._robot, -0.05, speed = 200, accuracy = 0.01)
+            while not command.update():
+                pass
             if self._lastTurnWasRight:
                 #käänny vasemmalle noin 20 astetta
+                command = RobotInterface.GyroPivot(self._robot, 20, speed = 150, accuracy = 1)
+                while not command.update():
+                    pass
                 self._lastTurnWasRight = False
             else:
                 #käänny oikealle noin 20 astetta
+                command = RobotInterface.GyroPivot(self._robot, -20, speed = 150, accuracy = 1)
                 self._lastTurnWasRight = True
         elif self._rightPushSensor.is_pressed():
             #aja taakse noin 5cm
+            command = RobotInterface.DriveForward(self._robot, -0.05, speed = 200, accuracy = 0.01)
+            while not command.update():
+                pass
             #käänny vasemmalle noin 10 astetta
-            pass
+            command = RobotInterface.GyroPivot(self._robot, 10, speed = 150, accuracy = 1)
+            while not command.update():
+                pass
         elif self._leftPushSensor.is_pressed():
             #aja taakse noin 5 cm
+            command = RobotInterface.DriveForward(self._robot, -0.05, speed = 200, accuracy = 0.01)
+            while not command.update():
+                pass
             #käänny oikealle noin 10 astetta
-            pass
+            command = RobotInterface.GyroPivot(self._robot, -10, speed = 150, accuracy = 1)
+            while not command.update():
+                pass
         else:
-            #aja eteenpäin noin 5 cm
-            pass
+            #aja eteen noin 10 cm
+            command = RobotInterface.DriveForward(self._robot, 0.10, speed = 200, accuracy = 0.01)
+            while not command.update():
+                pass
         return self.Id
 
     def Enter(self):
