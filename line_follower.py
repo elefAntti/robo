5a963a72928358d9c4b74dc2ba358030c5d8a728
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2018-10-20 13:22:33 +0300

    line following with one sensor

--- line_follower.py
+++ line_follower.py
@@ -5,40 +5,49 @@ from time import sleep
 import socket
 import sys
 
-llight_port = 1
-rlight_port = 2
+#llight_port = 1
+#rlight_port = 2
 
-llight = ev3.ColorSensor(port=llight_port)
-rlight = ev3.ColorSensor(port=rlight_port)
+#llight = ev3.ColorSensor(port=llight_port)
+#rlight = ev3.ColorSensor(port=rlight_port)
+light = ev3.ColorSensor()
 button = ev3.Button()
-llight.mode = 'COL-REFLECT'
-rlight.mode = 'COL-REFLECT'
+#llight.mode = 'COL-REFLECT'
+#rlight.mode = 'COL-REFLECT'
+light.mode = 'COL-REFLECT'
 
-bigTurn = 40
-lilTurn = 20
-bigDiff = 130
-lilDiff = 60
+#bigTurn = 40
+#lilTurn = 20
+#bigDiff = 130
+#lilDiff = 60
+turn = 20
+bright = 60
 
 def spin_around():
     lmotor = ev3.LargeMotor('outA')
     rmotor = ev3.LargeMotor('outB')
     while not button.any():
-        ll = llight.value()
-        rl = rlight.value()
-        pos = ll-rl
-        print(ll, rl, pos)
+        #ll = llight.value()
+        #rl = rlight.value()
+        #pos = ll-rl
+        #print(ll, rl, pos)
+        print(light.value())
 
         tspd = -30
         rspd = 0
 
-        if (pos > bigDiff):
-            rspd = -bigTurn
-        elif (pos > lilDiff):
-            rspd = -lilTurn
-        if (pos < -bigDiff):
-            rspd = bigTurn
-        elif (pos < -lilDiff):
-            rspd = lilTurn
+        #if (pos > bigDiff):
+        #    rspd = -bigTurn
+        #elif (pos > lilDiff):
+        #    rspd = -lilTurn
+        #if (pos < -bigDiff):
+        #    rspd = bigTurn
+        #elif (pos < -lilDiff):
+        #    rspd = lilTurn
+        if light.value() > bright:
+            rspd = turn
+        else:
+            rspd = -turn
 
         right = (tspd+rspd)/2.0
         left = tspd-right
